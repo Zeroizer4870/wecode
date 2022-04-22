14f04692247c2e2124a9d8dfa2f82bf1846a5400
Hoon Gi Ahn <ahg4870@gmail.com>
2022-04-22 12:29:05 +0900

    fix: 함수 수정

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-let arr = new Array();
+let arr = [];
 
-for (var i = 1; i <= 50; i++) {
-  if (i % 2 == 0) {
-    arr.push(i);
+function even() {
+  for (var i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      arr.push(i);
+    }
   }
+  return arr;
 }
-
-console.log(arr);
